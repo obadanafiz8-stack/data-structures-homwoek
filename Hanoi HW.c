00af82724160f6a604f85c08bc643400d2480768
obadanafiz8-stack <obadanafiz8@gmail.com>
2026-01-02 18:20:58 +0300

    Hanoi HW.c

---
+++ Hanoi HW.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void hanoi(int n, char source, char target, char auxiliary) {
+    if (n == 1) {
+        printf("Move disk 1 from %c to %c\n", source, target);
+        return;
+    }
+    
+    hanoi(n-1, source, auxiliary, target);
+    printf("Move disk %d from %c to %c\n", n, source, target);
+    hanoi(n-1, auxiliary, target, source);
+}
+
+// Example usage:
+// hanoi(3, 'A', 'C', 'B');
